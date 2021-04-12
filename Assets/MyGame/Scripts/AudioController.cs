adeef8473edbd47cbb4ddc045b842d532c256e78
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-04-12 08:32:33 +0200

    Install SteamVR

--- Assets/MyGame/Scripts/AudioController.cs
+++ Assets/MyGame/Scripts/AudioController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
+
+    private int a = 4;
+    private int b = 5;
+    public bool debug = true;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (debug)
+        {
+            Debug.Log("a = " + a + " b = " + b);
+        }
     }
 
     // Update is called once per frame

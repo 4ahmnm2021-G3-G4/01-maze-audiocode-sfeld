e87bc5aeb91f7caf79730f92bfc96372c5f9b2b4
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-05-10 13:18:19 +0200

    add audio sources and finish blocking

---
+++ Assets/MyGame/Scripts/SoundTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundTest : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+
+    public void playSound(bool isPlaying)
+    {
+        if (isPlaying)
+            audioSource.Play();
+        else
+            audioSource.Stop(); 
+    }
+}

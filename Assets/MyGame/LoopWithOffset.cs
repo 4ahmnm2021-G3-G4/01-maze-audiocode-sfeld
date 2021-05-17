25b96792fe835c95dac63138fd48b2622e28c30c
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-05-17 10:17:10 +0200

    add all sound loops

---
+++ Assets/MyGame/LoopWithOffset.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class LoopWithOffset : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+    public float timeBetweenSounds; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+  
+      
+    void Start()
+    {
+        StartCoroutine(SoundLoop());
+    }
+
+    
+
+    //If you want to play it forever
+    IEnumerator SoundLoop()    {
+
+        while (true)
+        { //or for(;;){
+            yield return new WaitForSeconds(timeBetweenSounds);
+            audioSource.clip = audioClip; 
+            audioSource.Play();
+        }
+    }
+}

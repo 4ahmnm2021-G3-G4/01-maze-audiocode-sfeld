01b6c4d921e8fcb4294506a5105d6d5f1e863686
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-05-31 09:31:13 +0200

    add Door and fix compression

---
+++ Assets/MyGame/Scripts/AnimateDoor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateDoor : MonoBehaviour
+{
+
+    public Vector3 rotationStepSize;
+    public Vector3 closeRotation;
+
+    public float speed = 1.0F;
+    private float startTime;
+    public float Speed;
+    // Start is called before the first frame update
+
+    public void closeDoor()
+    {
+        
+        StartCoroutine(RotateDoor());
+    }
+    IEnumerator RotateDoor()
+    {
+        while(gameObject.transform.rotation.eulerAngles.y > closeRotation.y)
+        {
+            gameObject.transform.Rotate(rotationStepSize);
+            yield return 1; 
+        }
+
+    }
+}
---
+++ Assets/MyGame/Scripts/ThroableOrb.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThroableOrb : MonoBehaviour
+{
+    public AudioClip throwSound;
+    public AudioClip hitSound;
+
+    public GameObject target;
+    AudioSource source;
+
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>(); 
+    }
+    public void playThrowSound()
+    {
+        source.clip = throwSound;
+        source.Play(); 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == target)
+        {
+            print("collide with target");
+            source.clip = hitSound;
+            source.Play();
+
+        }
+        else
+            print("wrong collider"); 
+    }
+}

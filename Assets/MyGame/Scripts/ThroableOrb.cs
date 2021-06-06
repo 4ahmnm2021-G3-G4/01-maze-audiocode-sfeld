0bdb5190b21bd91bdeb3d674e562a21a78eaabef
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-06-07 00:21:58 +0200

    let Ball respawn when a shot is missed

--- Assets/MyGame/Scripts/ThroableOrb.cs
+++ Assets/MyGame/Scripts/ThroableOrb.cs
@@ -8,15 +8,18 @@ public class ThroableOrb : MonoBehaviour
     public AudioClip hitSound;
 
     public GameObject target;
+    public Vector3 defaultPos; 
     AudioSource source;
-
+    bool isBeingThrown = false; 
 
     private void Start()
     {
+        defaultPos = gameObject.transform.position; 
         source = GetComponent<AudioSource>(); 
     }
     public void playThrowSound()
     {
+        isBeingThrown = true;
         source.clip = throwSound;
         source.Play(); 
     }
@@ -30,7 +33,15 @@ public class ThroableOrb : MonoBehaviour
             source.Play();
 
         }
+        else if (isBeingThrown)
+        {
+            isBeingThrown = false;
+            gameObject.transform.position = defaultPos; 
+        }
         else
             print("wrong collider"); 
     }
+
+  
+    
 }

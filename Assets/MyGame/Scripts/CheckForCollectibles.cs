f8d257da9f909e7ca8c34302a2bc9e551621ad42
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-06-19 17:45:11 +0200

    add new lamp model

---
+++ Assets/MyGame/Scripts/CheckForCollectibles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForCollectibles : MonoBehaviour
+{
+    public GameObject[] spheres;
+
+
+    private void Start()
+    {
+        foreach (GameObject sphere in spheres)
+        {
+            sphere.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Lamp") 
+        {
+            Collector collector = other.GetComponent<Collector>();
+            
+            for (int i = 0; i < collector.ammountOfCollected; i++)
+            {
+                spheres[i].SetActive(true); 
+
+            }
+        }
+    }
+
+}

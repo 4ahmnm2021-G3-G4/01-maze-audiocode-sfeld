b5d059348d627ce8fe9806d2aac8820cc7387b58
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-06-21 01:27:05 +0200

    add koi interaction

--- Assets/MyGame/Scripts/Collector.cs
+++ Assets/MyGame/Scripts/Collector.cs
@@ -12,11 +12,23 @@ public class Collector : MonoBehaviour
         {
             ammountOfCollected++;
 
+            if(collision.transform.Find("Particle System"))
+            {
             Transform particles = collision.transform.Find("Particle System");
             particles.SetParent(transform); //parent paricles to the lamp so they'll follow the player;    
             particles.localScale = new Vector3(1, 1, 1); //correct for the scale change from parenting; 
+            }
+            else if (collision.transform.parent.Find("Kois"))
+            {
+                Transform kois = (collision.transform.parent.Find("Kois"));
+                kois.SetParent(transform);
+                kois.localScale = new Vector3(0.007812348f, 0.007812348f, 0.007812348f);  //these numbers were just kopied out of play mode
+                kois.localPosition = new Vector3(0.00728f, -0.00089f, -0.00401f);
+            }
 
-            Destroy(collision.gameObject);
+            Destroy(collision.gameObject); 
+
+
 
         }
     }

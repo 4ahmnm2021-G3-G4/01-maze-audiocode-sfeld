12c9606bec701ca341387bec89223c9add19d16e
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-06-20 23:20:24 +0200

    make moths follow player

--- Assets/MyGame/Scripts/Collector.cs
+++ Assets/MyGame/Scripts/Collector.cs
@@ -12,7 +12,13 @@ public class Collector : MonoBehaviour
         if (collision.gameObject.tag == "Collectable")
         {
             ammountOfCollected++;
+
+            Transform particles = collision.transform.Find("Particle System");
+            particles.SetParent(transform); //parent paricles to the lamp so they'll follow the player;    
+            particles.localScale = new Vector3(1, 1, 1); //correct for the scale change from parenting; 
+
             Destroy(collision.gameObject);
+
         }
     }
 
